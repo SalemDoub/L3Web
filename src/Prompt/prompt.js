c50fc0e5b420838bb65ea1fdc65c22b7f65798bd
BARDAGHJI_GEORGES <georges.bardaghji@etu.univ-amu.fr>
2020-03-19 15:58:06 +0100

    creation of promt directory and prompt.js file contanig render function

---
+++ src/Prompt/prompt.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Prompt extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+                <input type="text" name="guess" />
+
+            </div>
+
+        )
+
+    }
+
+}
