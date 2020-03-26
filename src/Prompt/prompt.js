0570d586ab29ccb1d9d8e9214f8b2bb0bdc7c1b5
SalemDoub <salem.doub@etu.univ-amu.fr>
2020-03-26 22:54:04 +0000

    Drawer et les Guessers ont des différents privilèges , les prompts apparaissent pour les guessers .

--- src/Prompt/prompt.js
+++ src/Prompt/prompt.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
+import './Prompt.css'
 
 export default class Prompt extends Component {
-
     render() {
-
         return (
-
-            <div>
-
+            <div className="guesser-prompt">
+                <span>Ta réponse doit être écrite en miniscule</span>
                 <input type="text" name="guess" />
-
+                <input type="submit" value="Guess" />
             </div>
-
         )
-
     }
-
 }
