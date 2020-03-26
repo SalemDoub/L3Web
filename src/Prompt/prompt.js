f17574cf311986393d3d66ac9ba475b16ced0f77
BARDAGHJI_GEORGES <georges.bardaghji@etu.univ-amu.fr>
2020-03-27 00:05:38 +0100

    jeu basique sur console logs

--- src/Prompt/prompt.js
+++ src/Prompt/prompt.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 
+import './Prompt.css'
 export default class Prompt extends Component {
 
-    render() {
-
-        return (
-
-            <div>
-
-                <input type="text" name="guess" />
-
-            </div>
-
-        )
+    makeGuess = e => {
+        e.preventDefault();
+        if(e.target.guess.value) {
+            this.props.handleGuess(e.target.guess.value);
+        }
 
     }
-
+    render() {
+        return (
+            <div className="guesser-prompt">
+                <span>Type your answer here, all words are lowercase</span>
+                <form onSubmit={this.makeGuess}>
+                    <input type="text" name="guess" />
+                    <input type="submit" value="Guess" />
+                </form>
+            </div>
+        )
+    }
 }
