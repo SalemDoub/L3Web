141e592092daa851f45d746f912b9e04d07a7a77
BARDAGHJI_GEORGES <georges.bardaghji@etu.univ-amu.fr>
2020-04-02 18:10:20 +0200

    modification dans login.js

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './Login.css'
 
 export default class Login extends Component {
     constructor() {
@@ -7,47 +8,49 @@ export default class Login extends Component {
         this.state = {
             isValid: true,
         }
-
     }
-
     compileForm = e => {
         e.preventDefault();
         let t = e.target;
         if (!t.username.value || !t.password.value) {
-
             this.setState({ isValid: false });
         } else {
-
             let returnedObj = {
                 username: t.username.value,
                 password: t.password.value
             };
-
             this.props.handleLogin(returnedObj);
             this.setState({ isValid: true });
             this.props.history.push("/roomlist");
         }
-
     }
-
     render() {
         return (
-            <div>
+            <div className="login-container">
                 {this.state.isValid ? "" : <p>Incorrect</p>}
-                <form onSubmit={this.compileForm}>
-
+                <form className="login-form" onSubmit={this.compileForm}>
                     <label>
                         Username:
                         <input type="text" name="username"/>
                     </label>
+
                     <label>
                         Password:
+
                         <input type="password" name="password"/>
+
                     </label>
-                    <input type="submit" value="Login" />
+
+                    <input className="submit" type="submit" value="Login" />
+
                 </form>
-                <span>Not a member? <Link to="/signup">Sign up!</Link></span>
+
+                <span className="login-signup">Not a member? <Link className="to-signup" to="/signup">Sign up!</Link></span>
+
             </div>
+
         )
+
     }
+
 }
