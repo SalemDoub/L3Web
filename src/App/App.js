4f28050111ac7f76d07bf61af08e5c10e261e364
BARDAGHJI_GEORGES <georges.bardaghji@etu.univ-amu.fr>
2020-04-02 17:25:19 +0200

    app.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,108 +2,173 @@ import React from 'react';
 import {Link, Route, Switch, Redirect} from 'react-router-dom'
 import axios from 'axios'
 import './App.css';
-import Canvas from '../Canvas/Canvas'
+// import Canvas from '../Canvas/Canvas'
+import Room from '../Room/Room'
+import RoomList from '../RoomList/RoomList'
 import Login from '../Login/Login'
 import Singup from '../Signup/Signup'
 
 const url = "http://localhost:8080"
-
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      username: sessionStorage.username,
-      isLoggedIn: false,
+
+    constructor() {
+        super()
+        this.state = {
+            username: sessionStorage.username,
+            isLoggedIn: false,
+        }
     }
-  }
 
-  handleSignup = obj => {
-    axios
-        .post(url + "/api/users/create", obj)
-        .then(res => {
-          console.log(res.data)
-          sessionStorage.username = res.data.username;
-          this.setState({
-            username: res.data.username
-          });
-        })
-        .then(_ => {})
-        .catch(err => console.log(err));
-  };
+    handleSignup = obj => {
+        axios
+            .post(url + "/api/users/create", obj)
+            .then(res => {
+                console.log(res.data)
+                sessionStorage.username = res.data.username;
+                this.setState({
+                    username: res.data.username
+                });
+            })
 
-  handleLogin = obj => {
-    console.log(obj)
-    axios
-        .post(url + "/api/users/username/" + obj.username, obj)
-        .then(res => {
-          console.log(res.data)
-          sessionStorage.username = res.data;
-          this.setState({
-            isLoggedIn: true,
-            username: res.data
-          });
-        })
-        .catch(err => console.log(err));
-  };
+            .then(_ => {})
+            .catch(err => console.log(err));
 
-  handleLogout = e => {
-    e.preventDefault();
-    this.setState({
-      isLoggedIn: false,
-      username: ""
-    });
-    sessionStorage.clear();
-    this.props.history.push("/login");
-  };
+    };
 
-  componentDidMount() {
-    if(this.state.username) {
-      this.setState({
-        isLoggedIn: true
-      })
+    handleLogin = obj => {
+        console.log(obj)
+        axios
+            .post(url + "/api/users/username/" + obj.username, obj)
+            .then(res => {
+                console.log(res.data)
+                sessionStorage.username = res.data;
+                this.setState({
+                    isLoggedIn: true,
+                    username: res.data
+
+                });
+
+            })
+
+            .catch(err => console.log(err));
+    };
+
+    handleLogout = e => {
+        e.preventDefault();
+        this.setState({
+            isLoggedIn: false,
+            username: ""
+        });
+        sessionStorage.clear();
+        this.props.history.push("/login");
+    };
+
+    componentDidMount() {
+        if(this.state.username) {
+            this.setState({
+                isLoggedIn: true
+            })
+        }
     }
-  }
 
-  render() {
-    return (
-        <div className="App">
-          <nav className="navbar-container">
-            <h2>Acceuil</h2>
-            <span>Login</span>
-            {!this.state.isLoggedIn && (
-                <Link to="/login" className="nav-buttons">
-                  Login
-                </Link>
-            )}
-            {!this.state.isLoggedIn && (
-                <Link to="/signup" className="nav-buttons">
-                  Signup
-                </Link>
-            )}
-            {this.state.isLoggedIn && (
-                <Link to="/user">
+
+
+    render() {
+
+        return (
+
+            <div className="App">
+
+                <nav className="navbar-container">
+
+                    <div className="title-container">
+
+                        <span>Pictionary</span>
+
+                    </div>
+
+                    <div className="links-container">
+
+                        {!this.state.isLoggedIn && (
+
+                            <Link to="/login" className="nav-buttons">
+
+                                Login
+
+                            </Link>
+
+                        )}
+
+                        {!this.state.isLoggedIn && (
+
+                            <Link to="/signup" className="nav-buttons">
+
+                                Signup
+
+                            </Link>
+
+                        )}
+
+                        {this.state.isLoggedIn && (
+
+                            <Link to="/user">
+
                 <span className="nav-greeting">
-                  {"Hello, " + this.state.username}
-                </span>
-                </Link>
-            )}
-            {this.state.isLoggedIn && (
-                <Link onClick={this.handleLogout} to="/" className="nav-buttons">
-                  Logout
-                </Link>
-            )}
-          </nav>
-          {/* <Canvas /> */}
 
-          <Switch>
-            {this.state.isLoggedIn ? <Route path="/" exact render={(props) => <Login handleLogin={this.handleLogin} isLoggedIn={this.state.isLoggedIn} {...props} />} />:
-                <Route path="/" exact render={(props) => <Redirect to="/room" {...props}/> } />}
-            <Route path="/" exact redirect={(props) => <Redirect to="/login" {...props}/> } />
-              <Route path="/room" render={(props) => <Canvas username={this.state.username} {...props}/>} />
-            <Route path="/signup" render={(props) => <Singup handleSignup={this.handleSignup} {...props} />}  />
-            <Route path="/login" render={(props) => <Login handleLogin={this.handleLogin} {...props} />}  />
-          </Switch>
-        </div>
-    )
-  }
+                  {"Hello, " + this.state.username}
+
+                </span>
+
+                            </Link>
+
+                        )}
+
+                        {this.state.isLoggedIn && (
+
+                            <Link onClick={this.handleLogout} to="/" className="nav-buttons">
+
+                                Logout
+
+                            </Link>
+
+                        )}
+
+                    </div>
+
+
+
+                </nav>
+
+                {/* <Canvas /> */}
+
+
+
+                <Switch>
+
+                    {this.state.isLoggedIn ? <Route path="/" exact render={(props) => <Login handleLogin={this.handleLogin} isLoggedIn={this.state.isLoggedIn} {...props} />} />:
+
+                        <Route path="/" exact render={(props) => <Redirect to="/roomlist" {...props}/> } />}
+
+                    <Route path="/" exact redirect={(props) => <Redirect to="/login" {...props}/> } />
+
+                    <Route path="/room/:id" render={(props) => <Room username={this.state.username} {...props}/>} />
+
+                    <Route path="/roomlist" render={(props) => <RoomList username={this.state.username} {...props}/>} />
+
+                    <Route path="/signup" render={(props) => <Singup handleSignup={this.handleSignup} {...props} />}  />
+
+                    <Route path="/login" render={(props) => <Login handleLogin={this.handleLogin} {...props} />}  />
+
+                </Switch>
+
+            </div>
+
+        )
+
+    }
+
 }
+
+
+
+export default App;
